cdc48fbc28c10d2bb858620ae893b0b4064db3fb
Eslamahmed007 <136971624+Eslamahmed007@users.noreply.github.com>
2025-07-03 22:07:29 +0000

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-# توكنات البوتات
+PRE_BOT_TOKEN = "7228712143:AAGjZXlM_i2nNI6xsTvRgbokge1o9lQjf-8"
+PRE_CHAT_ID = "5660125152"
+
 ALEX_BOT_TOKEN = "8020725694:AAHVb-njS2E9cTWmZXdNxUjBq3m58tlFH_A"
 ALEX_CHAT_ID = "5660125152"
 
@@ -18,18 +20,27 @@ def send_telegram(token, chat_id, message):
 def format_order_message(order):
     order_number = order.get("order_number")
     total_price = order.get("total_price") + " EGP"
-    phone = order.get("shipping_address", {}).get("phone", "").replace("+", "").replace(" ", "")
-    
+
+    note_attrs = order.get("note_attributes", [])
+    phone = ""
+    for attr in note_attrs:
+        if attr.get("name") == "Whatsapp number (IMPORTANT)":
+            phone = attr.get("value", "").replace("+", "").replace(" ", "")
+            break
+
+    phone1 = order.get("shipping_address", {}).get("phone", "").replace("+", "").replace(" ", "")
+
     line_items = order.get("line_items", [])
     products = ""
     for item in line_items:
         products += f"- {item['title']} (x{item['quantity']})\n"
 
-    msg = f"""wa.me/{phone}
+    msg = f"""Whatsapp number ={phone}
+phone number= {phone1}
 
 안녕하세요!  to Korean Beautys   🌸  
 
-بنتواصل مع حضرتك لتأكيد طلبك رقم 007{order_number}
+بنتواصل مع حضرتك لتأكيد طلبك رقم {order_number}
 
 بمبلغ {total_price}
 
@@ -49,21 +60,35 @@ def format_order_message(order):
     
     return msg
 
-
 @app.post("/webhook")
 async def handle_order(request: Request):
     data = await request.json()
-
-    province = (
-        data.get("shipping_address", {}).get("province", "") or
-        data.get("billing_address", {}).get("province", "")
-    ).lower()
-
     message = format_order_message(data)
+    paid = data.get("financial_status", "")
+    codes = data.get("discount_codes", [])
+    code_values = [c.get("code", "") for c in codes]
+    codes_text = ", ".join(filter(None, code_values)) or "NO"
+    if "PREPAID" in codes_text:
+        send_telegram(PRE_BOT_TOKEN, PRE_CHAT_ID, message)
+        return {"status": "sent to prepaid bot"}
+    
+    elif paid=="Paid" or paid=="paid":
 
-    if "alexandria" in province:
-        send_telegram(ALEX_BOT_TOKEN, ALEX_CHAT_ID, message)
+        return {"status": "prepaid - skipped"}
+    
+
     else:
-        send_telegram(OTHER_BOT_TOKEN, OTHER_CHAT_ID, message)
 
-    return {"status": "sent"}
+
+        province = (
+            data.get("shipping_address", {}).get("province_code", "") or
+            data.get("billing_address", {}).get("province_code", "")
+        ).lower()
+
+
+        if "alx" in province:
+            send_telegram(ALEX_BOT_TOKEN, ALEX_CHAT_ID, message)
+        else:
+            send_telegram(OTHER_BOT_TOKEN, OTHER_CHAT_ID, message)
+
+        return {"status": "sent"}
